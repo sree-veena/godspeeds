9f944d608700386ccdbaaa0b03787188d4c53eff
sree-veena <122880687+sree-veena@users.noreply.github.com>
2023-07-10 22:16:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,161 +1,162 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
+const path = require('path');
 
-const {open} = require("sqlite");
+const { open } = require('sqlite');
 const sqlite3 = require('sqlite3').verbose();
 
-const app =express()
-app.use(express.json());
+const app = express();
+app.use(bodyParser.json());
 
-const db = new sqlite3.Database('restaurants.db',(err)=>{
-    if(!err){
-        console.log('Db Connected Successfully.')
-    }else{
-        console.log(err.message);
-    }
-});
+const dbPath = path.join(__dirname, 'restaurants.db');
 
+const initializeDBAndServer = async () => {
+  try {
+    const db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database
+    });
 
-const OwnerTable = `CREATE TABLE Owner (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE,
-    name TEXT
-  )`;
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS Owner (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE,
+        name TEXT
+      )
+    `);
 
-db.run(OwnerTable, (err)=>{
-    if(!err){
-        console.log('table successfully created');
-    }else{
-        console.log(err.message);
-    }
-})
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS Restaurant (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+        name TEXT,
+        since DATETIME,
+        isOpen BOOLEAN DEFAULT 0,
+        opsStartTime DATETIME,
+        opsEndTime DATETIME,
+        ownerId INTEGER,
+        slug TEXT UNIQUE,
+        description TEXT,
+        location TEXT
+      )
+    `);
 
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS Category (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+      )
+    `);
 
-const Restaurants = `
-CREATE TABLE Restaurant (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-  name TEXT,
-  since DATETIME,
-  isOpen BOOLEAN DEFAULT 0,
-  opsStartTime DATETIME,
-  opsEndTime DATETIME,
-  ownerId INTEGER,
-  slug TEXT UNIQUE,
-  description TEXT,
-  location TEXT
-)`;
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS MenuItems (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        description TEXT,
+        price INTEGER,
+        couponcode TEXT,
+        restaurant_id INTEGER,
+        FOREIGN KEY(restaurant_id) REFERENCES Restaurant(id) ON DELETE CASCADE
+      )
+    `);
 
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS Orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        fromRestaurant TEXT,
+        order_status TEXT DEFAULT 'NOT_INITIATED',
+        placedAt DATETIME,
+        fulfilledAt DATETIME,
+        orderItem TEXT
+      )
+    `);
 
-db.run(Restaurants, (err)=>{
-    if(!err){
-        console.log('restaurants table created');
-    }else{
-        console.log(err.message);
-    }
-})
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS OrderItem (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        menuItemId INTEGER,
+        quantity INTEGER,
+        orderId INTEGER,
+        FOREIGN KEY (menuItemId) REFERENCES MenuItems (id) ON DELETE CASCADE,
+        FOREIGN KEY (orderId) REFERENCES Orders (id) ON DELETE CASCADE
+      )
+    `);
 
-
-const Category = `CREATE TABLE Category(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name String
-)`;
-
-db.run(Category, (err)=>{
-    if(!err){
-        console.log('category table created');
-    }else{
-        console.log(err.message);
-    }
-})
-
-
-const MenuItems =`CREATE TABLE MenuItems(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name String,
-    description Strin,
-    price INTEGER,
-    couponcode String,
-    restaurant_id INTEGER,
-    FOREIGN KEY(restaurant_id) REFERENCES Restaurant(id) ON DELETE CASCADE
-    )`;
-
-db.run(MenuItems, (err)=>{
-    if(!err){
-        console.log('Menu Items table created');
-    }else{
-        console.log(err.message);
-    }
-})
-
-const Orders = `CREATE TABLE Orders(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    fromRestaurent String,
-    order_status TEXT DEFAULT 'NOT_INITIATED',
-    placedAt DATETIME,
-    fulfilledAt DATETIME,
-    orderItem TEXT
-    )`;
-
-db.run(Orders, (err)=>{
-    if(!err){
-        console.log('table created orders');
-    }else{
-        console.log(err.message);
-    }
-})
-
-
-const orderitems = `CREATE TABLE OrderItem(
-    id INTEGER PRIMARY KEY,
-    menuItemId INTEGER,
-    quantity INTEGER,
-    orderId INTEGER,
-    FOREIGN KEY(orderId) REFERENCES Orders(id) ON DELETE CASCADE,
-    FOREIGN KEY(menuItemId) REFERENCES MenuItems(id) ON DELETE CASCADE
-);`;
-
-db.run(orderitems, (err)=>{
-    if(!err){
-        console.log('order items table created');
-    }else{
-        console.log(err.message);
-    }
-})
-db.close();
-
-
-const dbPath = path.join(__dirname,'restaurants.db');
-
-let dbdata = null;
-
-const initializeDBAndServer = async () =>{
-    try{
-        dbdata = await open({
-            filename:dbPath,
-            driver: sqlite3.Database
-        })
-        app.listen('4000',(err)=>{
-            if(!err){
-                console.log('db connected successfully');
-            }else{
-                console.log(err.message);
-            }
-        })
-    }catch(e){
-        console.log(e.message);
-        process.exit(1);
-    }
-}
+    app.listen(3000, () => {
+      console.log('Server started on port 3000');
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
 initializeDBAndServer();
 
+app.post('/menuitems', async (req, res) => {
+    try {
+      const menuItemsDetails = req.body;
+      const db = await open({
+        filename: dbPath,
+        driver: sqlite3.Database
+      });
+    
+      const { name, description, price, couponcode, restaurant_id } = menuItemsDetails;
+  
+      const addMenuItemDetails = `
+        INSERT INTO MenuItems (name, description, price, couponcode, restaurant_id)
+        VALUES (?, ?, ?, ?, ?)  
+      `;
+  
+      await db.run(addMenuItemDetails, [name, description, price, couponcode, restaurant_id]);
+  
+      console.log('Menu item details inserted successfully');
+      res.send({ data: 'Successfully posted' });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ error: 'An error occurred' });
+    }
+  });
 
-app.post('/reataurantdetails', async(req,res)=>{
-    // const {RestaurantDetails} = ;
-    // console.log(RestaurantDetails);
-    const{
+  app.get('/menuitems', async (req, res) => {
+    try {
+      const db = await open({
+        filename: dbPath,
+        driver: sqlite3.Database
+      });
+  
+      const restaurants = await db.all('SELECT * FROM MenuItems');
+      res.send({ data: restaurants });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ error: 'An error occurred' });
+    }
+  });
+
+
+app.post('/restaurantdetails', async (req, res) => {
+  try {
+    const RestaurantDetails = req.body;
+    const {
+      createdAt,
+      name,
+      since,
+      isOpen,
+      opsStartTime,
+      opsEndTime,
+      ownerId,
+      slug,
+      description,
+      location,
+    } = RestaurantDetails;
+
+    const db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database
+    });
+
+    const addRestaurantDetails = `
+      INSERT INTO Restaurant (
         createdAt,
         name,
         since,
@@ -165,87 +166,161 @@ app.post('/reataurantdetails', async(req,res)=>{
         ownerId,
         slug,
         description,
-        location,
-    } = req.body;
-    const addRestaurantDetails = `INSERT INTO Restaurant(createdAt,name,since,isOpen,opsStartTime,opsEndTime,ownerId,slug,description,
-        location) VALUES ('${createdAt}',
-        '${name}',
-        '${since}',
-        '${isOpen}',
-        '${opsStartTime}',
-        '${opsEndTime}',
-        ${ownerId},
-        '${slug}',
-        '${description}',
-        '${location}');`;
-    
-const dbResponse = await db.run(addRestaurantDetails);
-res.json({data:'successfully post'});
-})
+        location
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
 
-app.get('/restaurants', async (req,res)=>{
-    const restaurants = `SELECT *FROM Restaurant;`;
-    const responsedb = await db.all(restaurants) ;
-    res.send({data:responsedb});
-})
+    await db.run(addRestaurantDetails, [
+      createdAt,
+      name,
+      since,
+      isOpen,
+      opsStartTime,
+      opsEndTime,
+      ownerId,
+      slug,
+      description,
+      location,
+    ]);
 
-app.get('/restaurants/:id', async (req,res)=>{
-    const restaurants = `SELECT *FROM Restaurant WHERE id = ${id}`;
-    const responsedb = await db.get(restaurants) ;
-    res.send({data:responsedb});
-})
+    console.log('Restaurant details inserted successfully');
+    res.send({ data: 'Successfully posted' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ error: 'An error occurred' });
+  }
+});
 
-app.put('/reataurant/:restaurantId', async(req, res)=>{
-const {restaurantId} = body.params;
-const restaurantDetails = req.body;
-const{
-    createdAt,
-    name,
-    since,
-    isOpen,
-    opsStartTime,
-    opsEndTime,
-    ownerId,
-    slug,
-    description,
-    location,
-} = restaurantDetails;
+app.get('/restaurants', async (req, res) => {
+  try {
+    const db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database
+    });
 
-const UpdateRestaurantDetails = `UPDATE Restaurant SET 
-createdAt = '${createdAt}',
-name ='${name}',
-since = '${since}',
-isOpen = '${isOpen}',
-opsStartTime = '${opsStartTime}',
-opsEndTime = '${opsEndTime}',
-ownerId = ${ownerId},
-slug = '${slug}',
-description = '${description}',
-location = '${location}' WHERE id = '${restaurantId}';
-`;
+    const restaurants = await db.all('SELECT * FROM Restaurant');
+    res.send({ data: restaurants });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ error: 'An error occurred' });
+  }
+});
 
-const responseupdate = await db.run(UpdateRestaurantDetails);
-res.send({data:'restaurant details updated successfully'});
-})
+app.get('/restaurants/:id', async (req, res) => {
+  try {
+    const db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database
+    });
 
-app.delete('restaurant/:restaurantId', async (req,res)=>{
-    const {delete_id}=  request.params;
+    const { id } = req.params;
+    const restaurant = await db.get('SELECT * FROM Restaurant WHERE id = ?', id);
+    res.send({ data: restaurant });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ error: 'An error occurred' });
+  }
+});
 
-    const deleteRestaurant = `DELETE *From restaurant WHERE id =${delete_id};`;
+app.put('/restaurant/:restaurantId', async (req, res) => {
+  try {
+    const db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database
+    });
 
-    await db.run(deleteRestaurant);
-    res.send({data:'data deleted successfully'});
-})
+    const { restaurantId } = req.params;
+    const restaurantDetails = req.body;
+    const {
+      createdAt,
+      name,
+      since,
+      isOpen,
+      opsStartTime,
+      opsEndTime,
+      ownerId,
+      slug,
+      description,
+      location,
+    } = restaurantDetails;
 
-app.post(`restaurant/search`, async (req,res)=>{
-    const searchCriteria = req.body;
+    const updateRestaurantDetails = `
+      UPDATE Restaurant SET 
+        createdAt = ?,
+        name = ?,
+        since = ?,
+        isOpen = ?,
+        opsStartTime = ?,
+        opsEndTime = ?,
+        ownerId = ?,
+        slug = ?,
+        description = ?,
+        location = ?
+      WHERE id = ?
+    `;
 
-    const query = `
-    SELECT * FROM Restaurant
-    WHERE name LIKE '%${searchCriteria.name}%'
-    OR location LIKE '%${searchCriteria.location}%'
-  `;
+    await db.run(updateRestaurantDetails, [
+      createdAt,
+      name,
+      since,
+      isOpen,
+      opsStartTime,
+      opsEndTime,
+      ownerId,
+      slug,
+      description,
+      location,
+      restaurantId,
+    ]);
 
-    const responseData = await db.all(query);
-    res.send({data:responseData});
-})
+    res.send({ data: 'Restaurant details updated successfully' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ error: 'An error occurred' });
+  }
+});
+
+app.delete('/restaurant/:restaurantId', async (req, res) => {
+  try {
+    const db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database
+    });
+
+    const { restaurantId } = req.params;
+
+    const deleteRestaurant = `DELETE FROM Restaurant WHERE id = ?`;
+
+    await db.run(deleteRestaurant, restaurantId);
+    res.send({ data: 'Data deleted successfully' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ error: 'An error occurred' });
+  }
+});
+
+app.post('/search/', async (req, res) => {
+    try {
+      const db = await open({
+        filename: dbPath,
+        driver: sqlite3.Database
+      });
+  
+      const { couponCode, location } = req.body;
+  
+      const query = `
+        SELECT * FROM MenuItems
+        INNER JOIN Restaurant ON MenuItems.restaurant_id = Restaurant.id
+        WHERE MenuItems.couponcode = ?
+        OR Restaurant.location LIKE '%' || ? || '%'
+      `;
+  
+      const responseData = await db.all(query, couponCode, location);
+      res.send({ data: responseData });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ error: 'An error occurred' });
+    }
+  });
+  
+  
